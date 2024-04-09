2c66bd5a34b8589d1acb0891404cc07f52894cb8
Mees Fix <mfix@stsci.edu>
2024-04-09 09:55:37 -0700

    updating exposure frame code and restructuring objects

--- jwst_rogue_path_tool/plotting.py
+++ jwst_rogue_path_tool/plotting.py
@@ -1,21 +1,21 @@
-import matplotlib as mpl
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 from matplotlib.patches import PathPatch, Wedge
 from matplotlib.path import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from pysiaf.utils import rotations
 
-from jwst_rogue_path_tool.utils import get_consecutive_valid_angles, get_intersecting_angles
-
 
 def create_exposure_plots(program, ncols=3):
-    """Generate exposure level plots
-    """
+    """Generate exposure level plots"""
     wedge_length = program.catalog_inner_radius - 1
 
     for obs_id in program.observation_exposure_combos:
         # Dynamically calculate number of rows based on ncols
-        nrows = len(program.observation_exposure_combos[obs_id]) // ncols + (len(program.observation_exposure_combos[obs_id]) % ncols > 0)
+        nrows = len(program.observation_exposure_combos[obs_id]) // ncols + (
+            len(program.observation_exposure_combos[obs_id]) % ncols > 0
+        )
 
         for n, exp_num in enumerate(program.observation_exposure_combos[obs_id]):
             exposure = program.exposure_frames[obs_id][exp_num]
@@ -27,21 +27,30 @@ def create_exposure_plots(program, ncols=3):
                 continue
 
             ax = plt.subplot(nrows, ncols, n + 1)
-            ax.set_xlabel('RA [Degrees]')
-            ax.set_ylabel('DEC [Degrees]')
-            ax.set_title('Observation {}, Exposure: {}'.format(obs_id, exp_num))
-            ax.scatter(program.ra, program.dec, marker='X', c='red')
-            ax.scatter(program.plotting_catalog['ra'],
-                       program.plotting_catalog['dec'], c='deeppink')
+            ax.set_xlabel("RA [Degrees]")
+            ax.set_ylabel("DEC [Degrees]")
+            ax.set_title("Observation {}, Exposure: {}".format(obs_id, exp_num))
+            ax.scatter(program.ra, program.dec, marker="X", c="red")
+            ax.scatter(
+                program.plotting_catalog["ra"],
+                program.plotting_catalog["dec"],
+                c="deeppink",
+            )
 
             ax.invert_xaxis()
 
             for angles in consecutive_angles:
                 min_theta = min(angles)
                 max_theta = max(angles)
-                w = Wedge((program.ra, program.dec), wedge_length, min_theta, 
-                        max_theta, fill=False, color='darkseagreen', 
-                        joinstyle='round')
+                w = Wedge(
+                    (program.ra, program.dec),
+                    wedge_length,
+                    min_theta,
+                    max_theta,
+                    fill=False,
+                    color="darkseagreen",
+                    joinstyle="round",
+                )
                 ax.add_artist(w)
 
             for angle in valid_angles:
@@ -56,37 +65,41 @@ def create_exposure_plots(program, ncols=3):
 
 
 def create_observation_plot(program, obs_id):
-    """Plot that describe all valid angles at the observation level
-    """
+    """Plot that describe all valid angles at the observation level"""
 
     obs_level_valid_angles = []
     wedge_length = program.catalog_inner_radius - 1
 
     for exp_num in program.exposure_frames[obs_id]:
         exposure = program.exposure_frames[obs_id][exp_num]
-        exposure.get_valid_angles(program.angle_step)
-        valid_angles = exposure.valid_angles
+        valid_angles = exposure.consecutive_angles
         obs_level_valid_angles.append(valid_angles)
 
-    intersecting_angles = get_intersecting_angles(obs_level_valid_angles)
-
-    obs_level_consecutive_angles = get_consecutive_valid_angles(intersecting_angles, program.angle_step)
+    obs_level_consecutive_angles = np.unique(obs_level_valid_angles)
 
     ax = plt.subplot()
-    ax.set_xlabel('RA [Degrees]')
-    ax.set_ylabel('DEC [Degrees]')
-    ax.set_title('Observation Level Plot')
-    ax.scatter(program.ra, program.dec, marker='X', c='red')
-    ax.scatter(program.plotting_catalog['ra'],
-            program.plotting_catalog['dec'], c='deeppink')
+    ax.set_xlabel("RA [Degrees]")
+    ax.set_ylabel("DEC [Degrees]")
+    ax.set_title("Observation Level Plot")
+    ax.scatter(program.ra, program.dec, marker="X", c="red")
+    ax.scatter(
+        program.plotting_catalog["ra"], program.plotting_catalog["dec"], c="deeppink"
+    )
 
     ax.invert_xaxis()
 
     for angles in obs_level_consecutive_angles:
         min_theta = min(angles)
         max_theta = max(angles)
-        w = Wedge((program.ra, program.dec), wedge_length, min_theta, max_theta, 
-                fill=False, color='darkseagreen', joinstyle='round')
+        w = Wedge(
+            (program.ra, program.dec),
+            wedge_length,
+            min_theta,
+            max_theta,
+            fill=False,
+            color="darkseagreen",
+            joinstyle="round",
+        )
         ax.add_artist(w)
 
     plt.tight_layout()
@@ -94,7 +107,6 @@ def create_observation_plot(program, obs_id):
 
 
 def get_susceptibility_region_patch(exposure):
-
     region = exposure.sus_reg
 
     patches = []
@@ -103,14 +115,40 @@ def get_susceptibility_region_patch(exposure):
         module = region[key]
         attitude = exposure.attitude
 
-        v2 = 3600*module.V2V3path.vertices.T[0] # degrees --> arcseconds
-        v3 = 3600*module.V2V3path.vertices.T[1] # degrees --> arcseconds
+        v2 = 3600 * module.V2V3path.vertices.T[0]  # degrees --> arcseconds
+        v3 = 3600 * module.V2V3path.vertices.T[1]  # degrees --> arcseconds
 
-        ra_rads, dec_rads = rotations.tel_to_sky(attitude, v2, v3) # returns ra and dec in radians
-        ra_deg, dec_deg = ra_rads*180./np.pi, dec_rads*180./np.pi # convert to degrees
+        ra_rads, dec_rads = rotations.tel_to_sky(
+            attitude, v2, v3
+        )  # returns ra and dec in radians
+        ra_deg, dec_deg = (
+            ra_rads * 180.0 / np.pi,
+            dec_rads * 180.0 / np.pi,
+        )  # convert to degrees
 
         ra_dec_path = Path(np.array([ra_deg, dec_deg]).T, module.V2V3path.codes)
         ra_dec_patch = PathPatch(ra_dec_path, lw=2, alpha=0.1)
         patches.append(ra_dec_patch)
-        
+
     return patches
+
+
+def locate_targets_in_annulus(self, inner_radius, outer_radius):
+    """Calculate the targets from a catalog that fall within inner and outer radii."""
+
+    # Set coordinates for target and catalog
+    target_coordinates = SkyCoord(self.ra * u.deg, self.dec * u.deg, frame="icrs")
+    catalog_coordinates = SkyCoord(
+        self.catalog["ra"].values * u.deg,
+        self.catalog["dec"].values * u.deg,
+        frame="icrs",
+    )
+
+    # Calculate separation from target to all targets in catalog
+    separation = target_coordinates.separation(catalog_coordinates)
+    mask = (separation.deg < outer_radius) & (separation.deg > inner_radius)
+
+    # Retrieve all targets in masked region above.
+    self.catalog_inner_radius = inner_radius
+    self.catalog_outer_radius = outer_radius
+    self.plotting_catalog = self.catalog[mask]
--- jwst_rogue_path_tool/utils.py
+++ jwst_rogue_path_tool/utils.py
@@ -3,34 +3,13 @@ from itertools import groupby
 
 import numpy as np
 
-def get_consecutive_valid_angles(angles, step):
-    """Find consecutive angle values that aren't larger than step.
-    """
-    window_indices = []
-    try:
-        window_start_index = angles[0]
-    except IndexError:
-        return None
-    for x,y in zip(angles[::], angles[1::]):
-        difference = y - x
-        if difference > step:
-            window_end_index = x
-            if window_end_index == window_start_index:
-                window_start_index = y
-                continue
-            else:
-                window_indices.append((window_start_index, window_end_index))
-                window_start_index = y
-        if y == angles[-1]:
-            window_indices.append((window_start_index, y))
-        else:
-            continue
 
-    return window_indices
+def get_valid_angles_windows(angles):
+    change = np.where(angles[:-1] != angles[1:])[0]
+    if change.size >0:
+        if angles[change[0]]:
+            change = np.roll(change,1)
+            window_start = angles[change[::2]]
+            window_end = angles[change[1::2]]
 
-def get_intersecting_angles(angles):
-    """Return intersecting angles from list of arrays of angles.
-    """
-    intersecting_angles = reduce(np.intersect1d, angles)
-
-    return intersecting_angles
+    return window_start, window_end
