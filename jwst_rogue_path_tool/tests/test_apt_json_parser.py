c6770eed093101e319f1a3b286b01125795e1974
Mees Fix <mfix@stsci.edu>
2025-02-28 16:50:17 -0500

    code style

--- jwst_rogue_path_tool/tests/test_apt_json_parser.py
+++ jwst_rogue_path_tool/tests/test_apt_json_parser.py
@@ -1,4 +1,5 @@
 """Unit tests for apt_json_parser"""
+
 import pytest
 
 from pathlib import Path
@@ -8,22 +9,25 @@ from jwst_rogue_path_tool.constants import PROJECT_DIRNAME
 
 JSON_PATH = Path(PROJECT_DIRNAME) / "data" / "APT_test_4RPtool.json"
 
+
 def test_json_exists(json_path=JSON_PATH):
-    """ Assert json test file exists
-    """
+    """Assert json test file exists"""
     assert json_path.exists()
+
 
 def test_load_json(json_path=JSON_PATH):
     program = aptJsonFile(json_path)
     assert program is not None
 
+
 @pytest.mark.parametrize(
     "tablename",
-    ["exposures",
-     "visit",
-     "observation",
-     "program",
-     "fixed_target",
+    [
+        "exposures",
+        "visit",
+        "observation",
+        "program",
+        "fixed_target",
     ],
 )
 def test_tables_exist(tablename, json_path=JSON_PATH):
